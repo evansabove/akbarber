fa261879d6360564ee5f07ebb928017b036d646b
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-15 18:19:38 +0000

    More info on about page

--- about.php
+++ about.php
@@ -2,8 +2,11 @@
 <?php include('navbar.php') ?>
 
 <div id="main">
-	<p>Mens Traditional Cuts</p>
-	<p>Shaves</p>
+	<p><b>AK Barber</b> is a mens barber shop based in Bath. We specialise in:
+	<ul>
+		<li>Mens Traditional Cuts</li>
+		<li>Shaves</li>
+	</ul>
 </div>
 
 <?php include('scripts.php') ?>
