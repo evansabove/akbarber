5ff558318096589cb2ea65f8f4dfde387bbb6aff
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-15 15:58:17 +0000

    Adding facebook link

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
-			<!--<div class="panel panel-default footer">
-				<div class="panel-body">
-			    	<span class="pull-right">&copy A K Barber <?php echo date("Y"); ?></span>
-				</div>
-			</div>-->
+			<div style="padding-top: 20px; padding-bottom: 20px">
+		    	<span class="pull-right">
+		    		<a href="https://www.facebook.com/Kala-Barber-525737427628260" target="_blank"><img src="img/fb.png" /></a>
+	    		</span>
+			</div>
 		</div>
 		<div class="col-xs-1"></div>
     </body>
