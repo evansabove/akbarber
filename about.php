39240a4adf27af46b7a4b44b6a429d9a665c7b92
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-15 15:34:20 +0000

    Adding missing files

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include('header.php') ?>
+<?php include('navbar.php') ?>
+
+<div id="main">
+	<p>Mens Traditional Cuts</p>
+	<p>Shaves</p>
+</div>
+
+<?php include('scripts.php') ?>
+<?php include('footer.php') ?>
