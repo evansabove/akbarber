29c3073f30eca716edd66444ade61c36f611fb36
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-15 18:12:09 +0000

    Adding image carousel

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 <?php include('navbar.php') ?>
 
 <div id="main">
-	<img src="img/ani.jpg" class="main-image"/>
+
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	  	<div class="carousel-inner" role="listbox">
+	    	<div class="item active">
+	      		<img src="img/carousel/slide1.jpg" class="main-image">
+	    	</div>
+
+		    <div class="item">
+			    <img src="img/carousel/slide2.jpg" class="main-image">
+	    	</div>
+
+	    	<div class="item">
+	    		<img src="img/carousel/slide3.jpg" class="main-image">
+	    	</div>
+
+	    	<div class="item">
+	    		<img src="img/carousel/slide4.jpg" class="main-image">
+	    	</div>
+	  	</div>
+	</div>
 </div>
 
 <?php include('scripts.php') ?>
