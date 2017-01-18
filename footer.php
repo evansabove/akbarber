9a430886d08aa104a0fde1c93bd3f26f81159eac
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2017-01-18 19:42:42 +0000

    Correcting facebook link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 			<div class="footer">
-	    		<a href="https://www.facebook.com/Kala-Barber-525737427628260" target="_blank"><img src="img/fb.png" /></a>
+	    		<a href="https://www.facebook.com/akbarberbath" target="_blank"><img src="img/fb.png" /></a>
 			</div>
 		</div>
 		<div class="col-xs-1"></div>
